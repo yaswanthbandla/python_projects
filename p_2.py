2dbb83d0169f96f75474d636b48623a923720ad2
yaswanth <yaswanthbandla82@gmail.com>
2023-03-03 11:06:46 +0530

    new_v

--- p_2.py
+++ p_2.py
@@ -1,12 +1,12 @@
 import random
-def guess_a_number(x):
-    ran_number=random.randint(1,x)
+def find_rand_num(x):
+    rand_num=random.randint(1,x)
     guess=0
-    while guess!=ran_number:
-        guess=int(input(f"guess a number betwwen 1 and {x}: "))
-        if guess<ran_number:
-            print("sorry.too low")
-        elif guess>ran_number:
-            print("sorry, too high")
-    print(f"wooah!!!!, you guessed correct it is {ran_number}")
-guess_a_number(50)
+    while guess!=rand_num:
+        guess=int(input())
+        if guess<rand_num:
+            print("too low.....")
+        elif guess>rand_num:
+            print("too high....")
+    print("Hurray!!!!!!!.....you guessed it correct")
+find_rand_num(50)
